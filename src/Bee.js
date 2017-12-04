03968bab2d03709f3e4128310d8e44e00223abdf
misha <mishall.afzal@gmail.com>
2017-12-04 12:34:54 -0800

    grub class functionality complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+    Grub.call(this, age, color, food, eat);
+    console.log(Bee);
 };
+
+// Bee.prototype = Object.create(Grub.prototype);
+
+// Bee.prototype.constructor = Bee;
+
+// Bee.age = 5;
+// Bee.color = 'yellow';
+// Bee.job = 'keep on growing';
 
